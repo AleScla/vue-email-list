96b7b90adaa5989f71ea42b9e408901ef1f3a5b7
AleScla <scla.alessandro@gmail.com>
2024-09-02 16:06:58 +0200

    esercizio completato con bonus

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,25 @@
-console.log('js funziona');
+const { createApp } = Vue
+
+createApp({
+  data() {
+    return {
+      eMails: []
+    }
+  },
+ 
+  created(){
+    for (let i = 0; i < 10; i++){
+        axios
+        .get('https://flynn.boolean.careers/exercises/api/random/mail')
+        .then((resp) => {
+            this.eMails.push(resp.data.response);  
+            
+        })       
+    }
+  }
+  
+}).mount('#app')
+
+
+
+
